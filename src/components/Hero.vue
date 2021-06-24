58b05dcca04424727786fb519f537616991807b1
grugknuckle <aaron.wolbach@gmail.com>
2021-06-23 21:24:28 -0400

    initial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center hero">
+    <img class="mb-3 app-logo" src="/Pizza42.svg" alt="Generic Pizza Logo" width="120" />
+    <h1 class="mb-4">Pizza 42</h1>
+    <p class="lead">
+      This is the Auth0 Solutions Engineer Technical Challenge application
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero"
+};
+</script>
