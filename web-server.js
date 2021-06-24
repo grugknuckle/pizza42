3b3d1fd923c1a2e06eb100254285a17b7c1d853c
grugknuckle <aaron.wolbach@gmail.com>
2021-06-23 21:46:11 -0400

    single quotes

--- web-server.js
+++ web-server.js
@@ -1,22 +1,22 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const helmet = require("helmet")
-const jwt = require("express-jwt")
-const jwksRsa = require("jwks-rsa")
-const { join } = require("path")
-const authConfig = require("./auth_config.json")
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
+const { join } = require('path')
+const authConfig = require('./auth_config.json')
 
 const app = express()
 
 if (!authConfig.domain || !authConfig.audience) {
-  throw "Please make sure that auth_config.json is in place and populated"
+  throw 'Please make sure that auth_config.json is in place and populated'
 }
 
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
-app.use(express.static(join(__dirname, "dist")))
+app.use(express.static(join(__dirname, 'dist')))
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -28,18 +28,18 @@ const checkJwt = jwt({
 
   audience: authConfig.audience,
   issuer: `https://${authConfig.domain}/`,
-  algorithms: ["RS256"]
+  algorithms: ['RS256']
 })
 
-app.get("/api/external", checkJwt, (req, res) => {
+app.get('/api/external', checkJwt, (req, res) => {
   res.send({
-    msg: "Your access token was successfully validated!"
+    msg: 'Your access token was successfully validated!'
   })
 })
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   app.use((_, res) => {
-    res.sendFile(join(__dirname, "dist", "index.html"))
+    res.sendFile(join(__dirname, 'dist', 'index.html'))
   })
 }
 
