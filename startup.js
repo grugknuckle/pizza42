a7685a592e3f37cd62b1c33dd304a7d5872cc1d8
grugknuckle <aaron.wolbach@gmail.com>
2021-06-25 00:54:01 -0400

    fix startup ports

--- startup.js
+++ startup.js
@@ -1,11 +1,29 @@
-if (process.env.NODE_ENV !== 'production') {
-  console.warn('loading environment variables from .env file ')
-  require('dotenv').config()
+const mode = process.argv[2]
+/**
+ * HEROKU BEST PRACTICES
+ * https://devcenter.heroku.com/articles/node-best-practices
+ */
+
+switch(mode) {
+  case 'dev':
+    require('dotenv').config()
+    process.env.PORT = 3001
+    process.env.NODE_ENV = 'development'
+    console.warn('Config loaded from local .env')
+    break;
+  case 'local':
+    require('dotenv').config()
+    process.env.PORT = 3000
+    process.env.NODE_ENV = 'local'
+    console.warn('Config loaded from local .env')
+    break
+  default:
+    // load from Heroku config vars
 }
 
-const app = require('./server')
 
-const port = (process.env.NODE_ENV === 'production') ? (process.env.PORT || 3000) : 3001
+const app = require('./server')
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.warn(`Express server started in ${process.env.NODE_ENV} environment`)
