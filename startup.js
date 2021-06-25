0202c2aaff69413b328d8f8b3ad5ec3320fae620
grugknuckle <aaron.wolbach@gmail.com>
2021-06-24 23:00:55 -0400

    lint no console.logs, use console.warn

--- startup.js
+++ startup.js
@@ -1,12 +1,12 @@
-if (process.env.NODE_ENV !== `production`) {
-  console.log(`loading environment variables from .env file `)
+if (process.env.NODE_ENV !== 'production') {
+  console.warn('loading environment variables from .env file ')
   require('dotenv').config()
 }
 
-const app = require("./server")
-const port = process.env.PORT || 3000
+const app = require('./server')
+const port = (process.env.NODE_ENV !== 'production') ? 3000 : 3001
 
 app.listen(port, () => {
-  console.log(`Express server started in ${process.env.NODE_ENV} environment`)
-  console.log(`Server listening on port ${port}.`)
+  console.warn(`Express server started in ${process.env.NODE_ENV} environment`)
+  console.warn(`Server listening on port ${port}.`)
 })
