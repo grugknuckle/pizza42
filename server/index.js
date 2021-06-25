e23c88a1b36cc40e0f3a17ee2833316296ee3123
grugknuckle <aaron.wolbach@gmail.com>
2021-06-25 00:32:18 -0400

    set env. remove err

--- server/index.js
+++ server/index.js
@@ -13,13 +13,6 @@ app.use(helmet())
 app.use(cors())
 app.use(serveStatic(join(__dirname, '../dist')))
 
-const domain = process.env.VUE_APP_AUTH0_DOMAIN
-const audience = process.env.VUE_APP_AUTH0_AUDIENCE
-
-if (!domain || !audience) {
-  throw 'The environment variables are not set properly !'
-}
-
 app.use('/api/external', checkJWT, require('./routes/external'))
 app.use('/api/v1/pizza', checkJWT, require('./routes/pizza'))
 
