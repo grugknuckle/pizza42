b0528a7d90246fd7a0a9b1cbbfb5d7693a18d9ea
grugknuckle <aaron.wolbach@gmail.com>
2021-06-26 12:29:22 -0400

    main.js use meta module

--- src/main.js
+++ src/main.js
@@ -3,28 +3,25 @@ import App from './App.vue'
 import Axios from './plugins/axios'
 import router from './router'
 import { Auth0Plugin } from './auth'
+import VueMeta from 'vue-meta'
 
 import hljs from 'highlight.js/lib/core'
 import 'highlight.js/styles/github.css'
 import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false
 
-Vue.use(hljs.vuePlugin)
+Vue.config.productionTip = false
 
 Vue.use(Auth0Plugin, {
   domain: process.env.VUE_APP_AUTH0_DOMAIN,
   clientId: process.env.VUE_APP_AUTH0_CLIENTID,
   audience: process.env.VUE_APP_AUTH0_AUDIENCE,
   onRedirectCallback: appState => {
-    router.push(
-      appState && appState.targetUrl
-        ? appState.targetUrl
-        : window.location.pathname
-    )
+    router.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname)
   }
 })
-
+Vue.use(VueMeta)
+Vue.use(hljs.vuePlugin)
 Vue.use(Axios)
 
 new Vue({
