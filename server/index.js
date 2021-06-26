b699651222c712a776546356d09dfc712bf63b9b
grugknuckle <aaron.wolbach@gmail.com>
2021-06-26 12:26:44 -0400

    enforce https in production only

--- server/index.js
+++ server/index.js
@@ -10,17 +10,18 @@ const checkJWT = require('./middleware/checkJWT')
 const enforceHTTPS = require('./middleware/enforceHTTPS')
 
 const app = express()
+const ENVIRON = process.env.NODE_ENV
 
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
-app.use(enforceHTTPS)
 app.use(serveStatic(join(__dirname, '../dist')))
 
 app.use('/api/external', checkJWT, require('./routes/external'))
 app.use('/api/v1/pizza', checkJWT, require('./routes/pizza'))
 
-if (process.env.NODE_ENV === 'production') {
+if (ENVIRON === 'production') {
+  app.use(enforceHTTPS)
   app.use((_, res) => {
     res.sendFile(join(__dirname, 'dist', 'index.html'))
   })
