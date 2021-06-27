b16d83efe1320285add992651e6e6a4638dd417e
grugknuckle <aaron.wolbach@gmail.com>
2021-06-27 13:02:15 -0400

    check 'scope' not 'scopes'

--- server/middleware/checkScopes.js
+++ server/middleware/checkScopes.js
@@ -1,7 +1,7 @@
 const checkJWTScopes = require('express-jwt-authz')
 
 module.exports = {
-  canReadOrders: checkJWTScopes([ 'read:orders' ], { customScopeKey: 'permissions' }),
-  canCreateOrders: checkJWTScopes([ 'create:orders' ], { customScopeKey: 'permissions' }),
-  hasUserScopes: checkJWTScopes([ 'openid', 'email', 'profile' ], { customScopeKey: 'scopes' })
+  canReadOrders: checkJWTScopes([ 'read:orders' ], { customScopeKey: 'scope' }),
+  canCreateOrders: checkJWTScopes([ 'create:orders' ], { customScopeKey: 'scope' }),
+  hasUserScopes: checkJWTScopes([ 'openid', 'email', 'profile' ], { customScopeKey: 'scope' })
 }
